6d24f41d7afe956723317e17ccde7d7fc49904d8
Alt+F4 <mail@example.com>
2022-04-20 17:56:35 +0200

    Wyczyszczenie

--- obecnosc/admin.py
+++ obecnosc/admin.py
@@ -5,13 +5,6 @@ class UczniowieAdmin(admin.ModelAdmin):
     list_display = ('id_ucznia', 'numer_w_dzienniku', 'nazwisko_imie', 'klasa')
     search_fields = ('id_ucznia', 'numer_w_dzienniku', 'nazwisko_imie', 'klasa')
     readonly_fields = ()
-    # asdas
-    # asdsadas
-<<<<<<< HEAD
-    #Test pobrania
-=======
-    #safasfasf
->>>>>>> 3757f716cefcc4980e468ca807a30a779035130d
 
     filter_horizontal = ()
     list_filter = ()
