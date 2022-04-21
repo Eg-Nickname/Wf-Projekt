cc0918439f9e20d36cb1928b134e3c240a36d8ae
Dimoon2134 <tymoteusz.lesniak1@gmail.com>
2022-04-21 12:17:36 +0200

    dodawanie obecności (do poprawienia)

--- obecnosc/views.py
+++ obecnosc/views.py
@@ -10,13 +10,19 @@ from .forms import ObecnoscForm
 
 def obecnosc_view(request):
     context={}
-    obj = Uczniowie.objects.none()
+    for id in request.POST:
+        if id != "csrfmiddlewaretoken" and id != "klasa":
+            uczen = Uczniowie.objects.get(id_ucznia=id)
+            status = request.POST.get(id)
+            Obecnosc = Obecnosci(uczen = uczen , status = status)
+            Obecnosc.save()
     form = ObecnoscForm(request.POST or None)
     if form.is_valid():
-        print(form.data['klasa'])
-        obj = Uczniowie.objects.filter(klasa=form.data['klasa'])    
+        obj = Uczniowie.objects.filter(klasa=form.data['klasa'])
+    else:
+        obj = Uczniowie.objects.none()    
     context={
-        "uczniowie" : obj,
+        'uczniowie' : obj,
         'form' : form
     }
     return render(request, 'obecnosc/obecnosc.html', context)
