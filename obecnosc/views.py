434b7cb1a27ccaf683a5a81784b795c4b875dc6a
Dimoon2134 <tymoteusz.lesniak1@gmail.com>
2022-04-21 10:37:53 +0200

    wypisywanie danych

--- obecnosc/views.py
+++ obecnosc/views.py
@@ -4,5 +4,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
 from django.contrib.auth.forms import  PasswordChangeForm
 
+from .models import Uczniowie, Obecnosci
+
 def obecnosc_view(request):
-    return render(request, 'obecnosc/obecnosc.html', {})
+    context={}
+    obj = Uczniowie.objects.filter()
+    context={
+        "uczniowie" : obj
+    }
+    return render(request, 'obecnosc/obecnosc.html', context)
