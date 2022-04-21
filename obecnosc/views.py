2d4fd3bae6e59517e970a892c6af78268fac4e97
Dimoon2134 <tymoteusz.lesniak1@gmail.com>
2022-04-21 11:12:20 +0200

    sortowanie w dzienniku

--- obecnosc/views.py
+++ obecnosc/views.py
@@ -6,10 +6,17 @@ from django.contrib.auth.forms import  PasswordChangeForm
 
 from .models import Uczniowie, Obecnosci
 
+from .forms import ObecnoscForm
+
 def obecnosc_view(request):
     context={}
-    obj = Uczniowie.objects.filter()
+    obj = Uczniowie.objects.none()
+    form = ObecnoscForm(request.POST or None)
+    if form.is_valid():
+        print(form.data['klasa'])
+        obj = Uczniowie.objects.filter(klasa=form.data['klasa'])    
     context={
-        "uczniowie" : obj
+        "uczniowie" : obj,
+        'form' : form
     }
     return render(request, 'obecnosc/obecnosc.html', context)
